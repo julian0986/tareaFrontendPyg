4191d119176f933006f7df2553cc81796b6d09ab
Julian Calle <julian0986@gmail.com>
2021-09-19 16:44:54 -0500

    Se realiza cambio de css

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,33 @@
 
 const nombrei = prompt("Ingrese Nombre:");
-const apellidoi = prompt("Ingrese apellido")
+const apellidoi = prompt("Ingrese apellido");
 
 /* Con Esta linea se crea un espa */
 document.getElementById('bienvenido').innerHTML = `Bienvenido: ${nombrei.charAt(0)}${apellidoi.charAt(0)}`
 
-let agregar = document.getElementById('guardar').value
-
-let paciente={}
-let array=[]
+let agregar = document.getElementById('guardar').value;
+let paciente={};
+let array=[]; 
 function pacientes(){
-        let valor =parseFloat( document.getElementById('valorcita').value,)
-        let iva =(valor*0.19)+valor
-        
+        let valor =parseFloat( document.getElementById('valorcita').value,);
+        let iva =(valor*0.19)+valor;
         paciente = {
-        cedula : document.getElementById('cedula').value,
-        nombre : document.getElementById('nombre').value,
-        apellido : document.getElementById('apellido').value,
-        edad : document.getElementById('edad').value,
-        celular : document.getElementById('celular').value,
-        correo: document.getElementById('Correo').value,
-        valorcita : valor,
-        historia : document.getElementById('historia').value,
-        citaiva :  iva,
+        Cedula : document.getElementById('cedula').value,
+        Nombre : document.getElementById('nombre').value,
+        Apellido : document.getElementById('apellido').value,
+        Edad : document.getElementById('edad').value,
+        Celular : document.getElementById('celular').value,
+        Correo: document.getElementById('correo').value,
+        Calorcita : valor,
+        Historia : document.getElementById('historia').value,
+        Citaiva :  iva,
 
     };
-    document.getElementById('respuesta').innerHTML = `Registraste a: ${paciente.nombre} ${paciente.apellido}`
-               
-    array.push(paciente)
-    console.table(array)
-    document.getElementById('foreset').reset()
-    
-}
+    document.getElementById('respuesta').innerHTML = `Registraste a: ${paciente.nombre} ${paciente.apellido}`;
+             
+    array.push(paciente);
+    console.table(array);
+    document.getElementById('foreset').reset();
+};
 
-pacientes()
-
-    
-
-
-
-
-
-
-
+pacientes();
