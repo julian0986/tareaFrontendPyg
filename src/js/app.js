b23e88fb1eaf58aa6e3fd3615191f78db25b010a
“Camilo <desarrolladordevops@gmail.com>
2021-09-18 19:16:58 -0500

    nos funciono el array

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,36 @@
-/* se crean dos cosntantes las cuales van ha ser ingresadas por teclado */
-const nombre = prompt("Ingrese Nombre:");
-const apellido = prompt("Ingrese apellido")
+
+const nombrei = prompt("Ingrese Nombre:");
+const apellidoi = prompt("Ingrese apellido")
+
 /* Con Esta linea se crea un espa */
-document.getElementById('bienvenido').innerHTML = `Bienvenido: ${nombre.charAt(0)}${apellido.charAt(0)}`
+document.getElementById('bienvenido').innerHTML = `Bienvenido: ${nombrei.charAt(0)}${apellidoi.charAt(0)}`
+
+let agregar = document.getElementById('guardar').value
+
+let paciente={}
+let array=[]
+function pacientes(){
+        let valor =parseFloat( document.getElementById('valorcita').value,)
+        let iva =(valor*0.19)+valor
+        paciente = {
+        cedula : document.getElementById('cedula').value,
+        nombre : document.getElementById('nombre').value,
+        apellido : document.getElementById('apellido').value,
+        edad : document.getElementById('edad').value,
+        celular : document.getElementById('celular').value,
+        correo: document.getElementById('Correo').value,
+        valorcita : valor,
+        historia : document.getElementById('historia').value,
+        citaiva :  iva,
+    };
+    array.push(paciente)
+    console.table(array)
+}
+pacientes()
+
+
+
+
+
+
+
