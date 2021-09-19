fde61a770711c544dbca4b6763a94cff27188e49
Julian Calle <julian0986@gmail.com>
2021-09-19 11:06:55 -0500

    Se agrega codigo para borrar el formulario y para que deje el mensaje de quien fue ingresado.

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ let array=[]
 function pacientes(){
         let valor =parseFloat( document.getElementById('valorcita').value,)
         let iva =(valor*0.19)+valor
+        
         paciente = {
         cedula : document.getElementById('cedula').value,
         nombre : document.getElementById('nombre').value,
@@ -22,11 +23,19 @@ function pacientes(){
         valorcita : valor,
         historia : document.getElementById('historia').value,
         citaiva :  iva,
+
     };
+    document.getElementById('respuesta').innerHTML = `Registraste a: ${paciente.nombre} ${paciente.apellido}`
+               
     array.push(paciente)
     console.table(array)
+    document.getElementById('foreset').reset()
+    
 }
+
 pacientes()
+
+    
 
 
 
